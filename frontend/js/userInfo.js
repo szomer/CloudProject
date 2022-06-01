f32efeb6fa8eb5c0818de22526fac5726a165582
Suzanne Zomer <suzannezomer@gmail.com>
2022-06-01 18:19:39 +0200

    Fix auth with token

--- frontend/js/userInfo.js
+++ frontend/js/userInfo.js
@@ -10,29 +10,8 @@ async function setUserInfo() {
   if (email == null || token == null || !email || !token) {
     console.log('err log in')
     document.querySelector(".nav-link-contact-link").innerHTML = 'Currently not logged in. <a href="/index.html" class="btn">Log In</a>';
-    // window.location.replace('./404noUser.html');
-    return;
-  }
-
-  try {
-    loggedIn = await (await fetch('/api/login', {
-      headers: {
-        'Authorization': 'Bearer ' + token,
-      }
-    }).then(res => res.json())
-      .then(data => obj = data)
-      .then(() => console.log(obj)));
-    console.log(loggedIn);
-  }
-  catch (ignore) { }
-
-
-  if (!loggedIn || loggedIn._error) {
-    console.log('err log in')
-    document.querySelector(".nav-link-contact-link").innerHTML = 'Currently not logged in. <a href="/index.html" class="btn">Log In</a>';
-    // window.location.replace('./404noUser.html');
-    return;
-
+    window.location.replace('./404noUser.html');
+    
   } else {
     console.log('logged in')
     document.querySelector(".nav-link-contact-link").innerHTML = `
